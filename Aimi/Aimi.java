b1f9151255c780a7cb91a1f9024e1582c8cff040
eddi <108631435+LowLevelLoser@users.noreply.github.com>
2023-09-07 18:55:57 -0500

    Add files via upload

---
+++ Aimi/Aimi.java
@@ -0,0 +1,16 @@
+class Aimi{
+    public static void main(String[] args) {
+        System.out.println("A file that big?");
+        System.out.println("It might be very useful.");
+        System.out.println("But now it is gone.");
+        System.out.println("--------------------");
+        System.out.println("The web site you seek");
+        System.out.println("cannot be located but");
+        System.out.println("countless more exist.");
+        System.out.println("--------------------");
+        System.out.println("Chaos reigns within.");
+        System.out.println("Refelect, repent, and reboot.");
+        System.out.println("Order shall return.");
+        System.out.println("--------------------");
+    }
+}
