c282db74228d689de153ad2412c3193a8c56a729
Svetlana <84134255+SvetlanaHoxha@users.noreply.github.com>
2023-09-12 19:48:57 -0500

    Arti - UIL 2021

--- Arti/Arti.java
+++ Arti/Arti.java
@@ -3,7 +3,21 @@ import java.util.Scanner;
 
 public class Arti {
     public static void main(String[] args){
-        String number = ("Arti.dat");
+        try{
+            Scanner reader = new Scanner("Arti.dat");
+            while (reader.hasNextDouble()){
+                double number = reader.nextDouble();
+
+                if (number >= 0) {
+                    double squareRoot = Math.sqrt(number);
+                    System.out.println("Square root of " + number + " is: " + squareRoot);
+                } else {
+                    System.out.println("Cannot calculate square root of a negative number: " + number);
+            }
+
+        } catch (Exception ohNo){
+            System.out.println("No file found");
+        }
 
         System.out.println("The square root of " + number + " is " + Math.sqrt(number));
 }
