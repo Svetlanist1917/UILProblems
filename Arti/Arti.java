d99acb854ed551c0f2a7e6228a79119a830f1a91
Svetlana <84134255+SvetlanaHoxha@users.noreply.github.com>
2023-09-12 19:48:40 -0500

    Arti - UIL 2021

---
+++ Arti/Arti.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Arti {
+    public static void main(String[] args){
+        String number = ("Arti.dat");
+
+        System.out.println("The square root of " + number + " is " + Math.sqrt(number));
+}
