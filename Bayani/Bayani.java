8e8a3314fde9add15db7b5d590b463b47b25831c
Svetlana <84134255+SvetlanaHoxha@users.noreply.github.com>
2023-09-19 19:39:30 -0500

    Add files via upload

---
+++ Bayani/Bayani.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+class Bayani {
+
+    public static void main(String[] args){
+        String fileName = "Bayani.dat";
+        List<Double> numbersList = new ArrayList<>();
+
+        try{
+            File data = new File(fileName);
+            Scanner reader = new Scanner(data);
+            
+            while (reader.hasNextLine()){
+                String line = reader.nextLine();
+
+                try{
+                    double number = Double.parseDouble(line);
+                    numbersList.add(number);
+                }catch(NumberFormatException ohNo){
+                    System.err.println("Error parsing a number.");
+                }
+            }
+            reader.close();
+
+            DecimalFormat df = new DecimalFormat("$ 0000.00");
+            double sum = 0.0;
+
+            for (Double number : numbersList) {
+                System.out.println(df.format(number));
+                sum += number;
+            }
+            System.out.println("Total = " + df.format(sum));
+
+        }catch (Exception ohNo){
+            System.err.println("No file Found!");
+        }
+    }
+}
