232f38c557c642ba3af91d5748ae498d68d0b571
eddi <108631435+LowLevelLoser@users.noreply.github.com>
2023-10-10 15:44:17 -0500

    Add files via upload

--- Bears/Bears.java
+++ Bears/Bears.java
@@ -1,22 +1,38 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.ArrayList; // import the ArrayList class
 
-public class Bears {
+class Bears {
     public static void main(String[] args) {
-        int bears = 0;
-
-            File data = new File("Bears.dat");
-            Scanner Reader = new Scanner(data);
-            while (Reader.hasNextLine()) {
-                String text = Reader.nextLine();
-                for (int index = 0; index < text.length(); index++) {
-                    if (text.charAt(index) == 'B') {
-                        count++;
-                }catch(Exception ohNo){
-                    System.err.println("No File Found!");
-                }
+        try {
+            File myFile = new File("bears.dat");
+            int bears = 0;
+            Scanner myScanner = new Scanner(myFile);
+            Boolean terminate = false;
+            ArrayList<String> lines = new ArrayList<String>();
+            while (myScanner.hasNextLine()){
+                lines.add(myScanner.nextLine());
             }
+            int index = 0;
+            int matrixes = Integer.parseInt(lines.get(index));
+            index++;
+            for (int i = 0; i < matrixes; i++){
+              for(int j = 0; j < 10; j++){
+                String googoo = lines.get(index);
+                  for (int k = 0; k < 10; k++) {
+                      if (googoo.charAt(k) == 'B') {
+                          bears++;
+                      }
+                  }
+                  index++;
+              }
+              System.out.println(bears);
+              bears = 0;
+            }
+            myScanner.close();
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 }
