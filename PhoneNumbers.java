4aab5147b99f68b2a5ef5d590df3d98df71418d3
AdamBauerle110 <145501879+AdamBauerle110@users.noreply.github.com>
2023-12-14 14:35:02 -0600

    Update PhoneNumbers.java

--- PhoneNumbers.java
+++ PhoneNumbers.java
@@ -27,13 +27,17 @@ public class PhoneNumbers {
     
     public static void main(String[] args){
         for(int line = 1; line <= Integer.parseInt(getLine(0)); line ++){
+            //Splits phone number into its smaller parts
             String[] number = getLine(line).split("-");
-            
+
+            //If an error occurs, line must not be a valid phone number
             try{
+                //Tries to turn each of the 3 seprate strings into integers
                 Integer.parseInt(number[0]);
                 Integer.parseInt(number[1]);
                 Integer.parseInt(number[2]);
-                
+
+                //Checks if each number is correct length, if so, output phone number and continueto next line
                 if(number.length == 3 && number[0].length() == 3 &&  number[1].length() == 3 && number[2].length() == 4){
                     System.out.println(getLine(line));
                     continue;
